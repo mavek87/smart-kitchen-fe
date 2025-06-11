8ac7b185be03aa42862597657d8c336d7af2a640
mavek87 <matver87@gmail.com>
2025-06-11 11:00:14 +0200

    removed useless file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import React from 'react'
-import App2 from "./App2.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App2 />
+        <App />
     </React.StrictMode>,
 );
