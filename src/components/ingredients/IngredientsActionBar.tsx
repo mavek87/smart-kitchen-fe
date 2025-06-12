90954494a29215f6f44aecfdddcc591617785208
mavek87 <matver87@gmail.com>
2025-06-12 10:48:25 +0200

    improved graphics of IngredientsActionBar

--- src/components/ingredients/IngredientsActionBar.tsx
+++ src/components/ingredients/IngredientsActionBar.tsx
@@ -15,21 +15,7 @@ export default function IngredientsActionBar({recipeId}: IngredientManagerProps)
     const deleteIngredientHandler = () => deleteRecipeByIdMutation.mutate(recipeId);
 
     return (
-        <div className="flex flex-row pl-5 gap-5">
-            <button onClick={() => setOpenModalDeleteIngredient(true)}>Delete Recipe</button>
-            <Modal
-                title={"Delete Recipe"}
-                content={
-                    <div>
-                        Are you sure you want to delete this recipe?
-                    </div>
-                }
-                isModalOpen={openModalDeleteIngredient}
-                onCloseModal={() => setOpenModalDeleteIngredient(oldValue => !oldValue)}
-                onCancelModal={() => setOpenModalDeleteIngredient(false)}
-                onConfirmModal={deleteIngredientHandler}
-            />
-
+        <div className="flex flex-col pl-5 gap-5">
             <button onClick={() => setOpenModalAddIngredient(true)}>Add Ingredient</button>
             <Modal
                 title={"Add ingredient"}
@@ -42,6 +28,20 @@ export default function IngredientsActionBar({recipeId}: IngredientManagerProps)
                 isModalOpen={openModalAddIngredient}
                 modalWidthSize={"xl"}
                 onCloseModal={() => setOpenModalAddIngredient(oldValue => !oldValue)}
+            />
+
+            <button onClick={() => setOpenModalDeleteIngredient(true)}>Delete Recipe</button>
+            <Modal
+                title={"Delete Recipe"}
+                content={
+                    <div>
+                        Are you sure you want to delete this recipe?
+                    </div>
+                }
+                isModalOpen={openModalDeleteIngredient}
+                onCloseModal={() => setOpenModalDeleteIngredient(oldValue => !oldValue)}
+                onCancelModal={() => setOpenModalDeleteIngredient(false)}
+                onConfirmModal={deleteIngredientHandler}
             />
         </div>
     );
